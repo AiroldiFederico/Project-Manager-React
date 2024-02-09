b17e0bc4bf6058d9c1fdd07b54602ed3f755a55c
Federico Airoldi <airoldibusiness@gmail.com>
2024-02-09 02:15:06 +0100

    funzione per creare i singoli task

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,13 +1,43 @@
+import {useState, useRef} from "react";
+
 
 
 export default function TaskManager() {
-	return <section className="mt-8">
+
+	const [task, setTask] = useState([])
+	const inputTask = useRef();
+
+	function handleTask() {
+		const newTask = inputTask.current.value;
+		if (newTask) { // Verifica che il nuovo task non sia vuoto
+			setTask(actualTask => [...actualTask, newTask]);
+			inputTask.current.value = ''; // Opzionale: pulisci l'input dopo l'aggiunta
+		}
+	}
+
+
+	return <section className="mt-8 font-semibold">
 		<h2 className="text-2xl font-bold text-stone-700 mb-4">Task</h2>
 
 		<div className="flex ">
-			<input type="text" className="w-64 px-2 py-1 rounded-sm bg-stone-200"/>
-			<button className="text-stone-700 hover:text-stone-950" >Add Task</button>
+			<input type="text"
+			       ref={inputTask}
+			       className="w-64 px-2 py-1 rounded-sm bg-stone-200"/>
+			<button
+				onClick={handleTask}
+				className="text-stone-700 hover:text-stone-950 ml-4" >Add Task
+			</button>
 		</div>
+
+		<ul className="p-2 mt-8 rounded-md bg-stone-100">
+			{task.map((value, indexValue) => (
+				<li key={indexValue} className="flex justify-between my-4">
+					<span>{value}</span>
+					<button className="text-stone-700 hover:text-red-500 me-2">Clear</button>
+				</li>
+			))}
+
+		</ul>
 
 	</section>
 }
